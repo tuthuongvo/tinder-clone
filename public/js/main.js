542a14b55366d1c6402fe24521b6aceae628a553
Thuong Vo <thuongvo@coffeemugvn.com>
2024-10-29 23:06:10 +0700

    fix bugs

--- public/js/main.js
+++ public/js/main.js
@@ -11,20 +11,53 @@ $(document).ready(function () {
       $(".close-modal").addClass("hidden");
     });
   }
-    const coverSlider = function () {
+  const coverSlider = function () {
         $(".cover-slider").slick({
           dots: true,
-        //   infinite: true,
-        //   speed: 500,
           slidesToShow: 1,
           slidesToScroll: 1,
-        //   autoplay: true,
-        //   autoplaySpeed: 2000,
           arrows: false,
         });
-    }
+  }
+  const toggleFilter = function() {
+    $(".item-title").click(function () {
+      $(this).next(".item-content").toggleClass("hidden");
+      $(this).find(".icon").toggleClass("rotate-90");
+    });
+  }
+  if ($(".cover-slider").length) { 
+    coverSlider();
+  }
+  
 
   toggleSearch();
   closeModal();
-  coverSlider();
+  toggleFilter();
 });
+
+  $(function () {
+    $(".radio-input").change(function () {
+       // Get the value of the selected radio input
+       const selectedValue = $(this).val();
+       // Find the <p> element with data-target attribute that matches the group
+       $(this).closest(".item").find(".amount").text(selectedValue);
+    s});
+    if ($("#slider-range").length) {
+      $("#slider-range").slider({
+        range: true,
+        min: 1,
+        max: 100,
+        values: [15, 80],
+        slide: function (event, ui) {
+          $("#amount").val(ui.values[0] + " - " + ui.values[1] + " tuổi");
+        },
+      });
+      $("#amount").val(
+        $("#slider-range").slider("values", 0) +
+          " - " +
+          $("#slider-range").slider("values", 1) +
+          " tuổi"
+      );
+    }
+    
+  });
